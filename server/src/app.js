0c42c600ed283116a0530d84fda0b74ef6dc95db
abh <ahmadbinhamid164@gmail.com>
2026-08-03 02:38:09 +0500

    allow X-tenant-slug

--- server/src/app.js
+++ server/src/app.js
@@ -40,7 +40,7 @@ app.use(
           }
         : true, // dev fallback: allow all
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Tenant-Slug"],
     credentials: true,
   })
 );
